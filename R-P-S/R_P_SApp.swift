1af6798e0f04fbb56967db6aa9ceab4b738ca486
Danielle Gibson <peyton.gibson@stu.barren.kyschools.us>
2024-11-06 09:01:51 -0600

    Initial Commit

---
+++ R-P-S/R_P_SApp.swift
@@ -0,0 +1,17 @@
+//
+//  R_P_SApp.swift
+//  R-P-S
+//
+//  Created by Student on 11/6/24.
+//
+
+import SwiftUI
+
+@main
+struct R_P_SApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ R-P-SUITests/R_P_SUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  R_P_SUITestsLaunchTests.swift
+//  R-P-SUITests
+//
+//  Created by Student on 11/6/24.
+//
+
+import XCTest
+
+final class R_P_SUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
