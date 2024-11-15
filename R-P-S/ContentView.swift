aede2e9172af959ed5139842d3c511a0d7475a6e
Danielle Gibson <peyton.gibson@stu.barren.kyschools.us>
2024-11-15 08:48:24 -0600

    completed

--- R-P-S/ContentView.swift
+++ R-P-S/ContentView.swift
@@ -7,18 +7,107 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+enum Choices:String, CaseIterable {
+    case Rock = "🪨", Paper = "📄", Scissors = "✂️"
 }
 
-#Preview {
-    ContentView()
+struct ContentView: View {
+    @State var computerChoice = Choices.allCases.first!
+    @State var gameOutcome = ""
+    
+    @State var wins = 0
+    @State var round = 0
+    
+    @State var showAlert = false
+    @State var showComputerChoice = false
+    
+    var body: some View {
+        GeometryReader{geo in
+            VStack {
+                //Computer
+                VStack{
+                    if !showComputerChoice {
+                        Text("🤖")
+                            .font(.system(size: 100))
+                    } else {
+                        Text(computerChoice.rawValue)
+                            .font(.system(size: 100))
+                    }
+                } .frame(width: geo.size.width, height: geo.size.height/2)
+                //Player
+                VStack {
+                    Text("Make your selection:")
+                    HStack(spacing:0){
+                        ForEach(Choices.allCases, id:\.self){option in
+                            Button(action:{
+                                // Start the round
+                                round += 1
+                                
+                                //Generate a computer choice
+                                let index = Int.random(in: 0...Choices.allCases.count-1)
+                                computerChoice = Choices.allCases[index]
+                                showComputerChoice = true
+                                
+                                //check if win
+                                checkWin(playerChoice: option)
+                            }){
+                                Text(option.rawValue)
+                                    .font(.system(size: geo.size.width/CGFloat(Choices.allCases.count)))
+                            }
+                        }
+                    }
+                    HStack{
+                        Spacer()
+                        Text("Wins: \(wins)")
+                        Spacer()
+                        Text("Round: \(round)")
+                        Spacer()
+                        
+                    }
+                }.frame(width: geo.size.width, height: geo.size.height/2)
+            }
+        }
+        .alert("You \(gameOutcome)!", isPresented: $showAlert){
+            Button("Play again!", role: .cancel){
+                showComputerChoice = false}
+        }
+    }
+    func checkWin(playerChoice:Choices){
+        switch playerChoice {
+        case .Scissors:
+            if computerChoice == .Scissors {
+                gameOutcome = "Draw"
+            } else if computerChoice == .Paper{
+                gameOutcome = "Win"
+                wins += 1
+            } else {
+                gameOutcome = "Lose"
+            }
+        case .Rock:
+            if computerChoice == .Rock {
+                gameOutcome = "Draw"
+            } else if computerChoice == .Scissors{
+                gameOutcome = "Win"
+                wins += 1
+            } else {
+                gameOutcome = "Lose"
+            }
+        case .Paper:
+            if computerChoice == .Paper {
+                gameOutcome = "Draw"
+            } else if computerChoice == .Rock{
+                gameOutcome = "Win"
+                wins += 1
+            } else {
+                gameOutcome = "Lose"
+            }
+            showAlert = true
+            
+            struct ContentView_Previews: PreviewProvider {
+                static var previews: some View {
+                    ContentView()
+                }
+            }
+        }
+    }
 }
